70211cc7f6631a27f5798d8099624f2823677236
Miumen <sharifigiv5954@gmail.com>
2023-03-28 11:52:19 +0330

    Add init vars

--- plot.cpp
+++ plot.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <functional>
+
 using namespace std;
 
 class Plot{
+    public:
+        int color[4];
 
+        function<int(int)> solver = NULL;
+
+        void draw(){
+            
+        };
 };
