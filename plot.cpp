5e85958a8fdeed96a279bcee99b901e834375964
Miumen <sharifigiv5954@gmail.com>
2023-03-28 12:25:06 +0330

    Add draw func

--- plot.cpp
+++ plot.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 #include <functional>
 
 using namespace std;
@@ -7,9 +8,15 @@ class Plot{
     public:
         int color[4];
 
-        function<int(int)> solver = NULL;
+        function<float(float)> solver = NULL;
 
         void draw(){
-            
+            float y = 0.1;
+
+            while (y < 500){
+                float res = solver(y);
+                
+                y += 0.1;
+            };
         };
 };
